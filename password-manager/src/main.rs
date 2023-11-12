20d34cdf2f82a893ca8cc0d14b92fd634f338e2a
Gupples <85248061+Gupples@users.noreply.github.com>
2023-11-11 18:16:01 -0700

    Removed CSV implementation

--- password-manager/src/main.rs
+++ password-manager/src/main.rs
@@ -5,34 +5,51 @@ use std::io;
 use std::cmp::Ordering;
 
 // Enable working with CSV's.
-use csv::ReaderBuilder;
+// use csv::ReaderBuilder;
 
 // Create a struct for the Password Manager.
 struct Manager {
     master_password: String,
     active: bool,
+    sites: Vec<String>,
+    usernames: Vec<String>,
+    passwords: Vec<String>
+
+
 }
 
 // Create methods for Manager.
 impl Manager{
-    fn set_master_password(&mut self) {
-        println!("Please enter your master password: ");
-        let mut password_input = String::new();
+    fn get_input(&self, prompt: String) -> String {
+        println!("{prompt}");
+        let mut input = String::new();
         io::stdin()
-            .read_line(&mut password_input)
+            .read_line(&mut input)
             .expect("Failed to read line!");
+        return input;
+    }
+
+    fn set_master_password(&mut self) {
+        let mut password_input = self.get_input(
+            "Please enter your master password: ".to_string());
         self.master_password = password_input;
     }
-    
-    fn get_permission(&self) {
-        println!("Please enter your master password: ");
-        let mut password_input = String::new();
-        io::stdin()
-            .read_line(&mut password_input)
-            .expect("Failed to read line!");
+
+    fn action(&self){
+
     }
 }
 fn main() {
-    println!("Hello, world!");
+    let password_manager = Manager{
+        master_password: String::from(""),
+        active: true,
+        sites: [].to_vec(),
+        usernames: [].to_vec(),
+        passwords: [].to_vec()
+        
+    };
+    while password_manager.active{
+        password_manager.action();
+    }
 }
 
