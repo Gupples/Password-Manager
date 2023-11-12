b93f634bb5ee97ccaacfe5117b61997952e7e901
Gupples <85248061+Gupples@users.noreply.github.com>
2023-11-11 19:21:33 -0700

    Updated methods

--- password-manager/src/main.rs
+++ password-manager/src/main.rs
@@ -1,3 +1,4 @@
+use core::panic;
 // import input/output from the standard library.
 use std::io;
 
@@ -20,7 +21,75 @@ struct Manager {
 
 // Create methods for Manager.
 impl Manager{
-    fn get_input(&self, prompt: String) -> String {
+    
+
+    pub fn quit(&mut self) {
+        self.active = false;
+    }
+
+
+
+    fn set_master_password(&mut self) {
+        let mut password_check = get_input(
+            "Please enter your master password: ".to_string());
+        
+        if password_check == self.master_password {
+            let mut new_password1 = get_input(
+                "Please enter your new master password: ".to_string());
+            let mut new_password2 = get_input(
+                "Please enter your new master password again: ".to_string());
+            if new_password1 == new_password2{
+                self.master_password = new_password1;
+                println!("Password successfully changed!");
+            } else {
+                println!("Your passwords did not match.");
+            }
+
+        } else {
+            println!("Incorrect master password.");
+        }
+        
+    }
+
+}
+fn main() {
+    let mut password_manager = Manager{
+        master_password: String::from(""),
+        active: true,
+        sites: [].to_vec(),
+        usernames: [].to_vec(),
+        passwords: [].to_vec()
+        
+    };
+
+    if get_input("Master Password: ".to_string())
+     != password_manager.master_password {
+         println!("Incorrect master password. ");
+         
+    } else {
+        while password_manager.active{
+                    let actions : Vec<&str> = [
+            // "1. View entries,",
+            // "2. Add entry,",
+            // "3. Edit entry,",
+            // "4. Delete entry,",
+            "5. Change Master Password",
+            "0. Quit,"].to_vec();
+            for item in actions {
+                println!("{item}");
+            }
+            let action = get_number_input("Choose an action: ".to_string());
+            match action {
+                0 => password_manager.quit(),
+                5 => password_manager.set_master_password(),
+                _ => panic!()
+                
+            }
+        }
+    }
+}
+
+fn get_input(prompt: String) -> String {
         println!("{prompt}");
         let mut input = String::new();
         io::stdin()
@@ -29,27 +98,15 @@ impl Manager{
         return input;
     }
 
-    fn set_master_password(&mut self) {
-        let mut password_input = self.get_input(
-            "Please enter your master password: ".to_string());
-        self.master_password = password_input;
+    fn get_number_input(prompt: String) -> u32 {
+        let mut is_number: bool = false;
+        let mut number = 0;
+        while !is_number {
+            let number = get_input(prompt.clone());
+            let number: u32 = match number.trim().parse(){
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+        }
+        return number;
     }
-
-    fn action(&self){
-
-    }
-}
-fn main() {
-    let password_manager = Manager{
-        master_password: String::from(""),
-        active: true,
-        sites: [].to_vec(),
-        usernames: [].to_vec(),
-        passwords: [].to_vec()
-        
-    };
-    while password_manager.active{
-        password_manager.action();
-    }
-}
-
